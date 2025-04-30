3c0f1068782084ff7922f59da1c2f07dc1be1c37
ayeushmaan1 <ayushmaandwivediiaf@gmail.com>
2025-04-30 12:25:02 +0000

    math and num

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,10 @@
+const score=400
+console.log(400)
+const balance=new Number(400)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance)
+const othernumber=123.8966
+console.log(othernumber.toPrecision(3))
+const hundreds=100000;
+console.log(hundreds.toLocaleString('en-IN'))
