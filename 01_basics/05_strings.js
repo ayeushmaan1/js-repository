2cc57262b40e757b3c917bdf66b30c7a9e90ff4e
ayeushmaan1 <ayushmaandwivediiaf@gmail.com>
2025-04-30 12:07:59 +0000

    new

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const gamename=new String("ayushmaan-dwivedi")
+console.log(gamename.charAt(3));
+const n=gamename.substring(0,4)
+console.log(n);
+ const anotherstring=gamename.slice(-8,4)
+ console.log(anotherstring);
+ const url="https:ayushmaan.com/ayushmaan%20choudahry"
+ console.log(url.replace('%20','-'))
+ console.log(gamename.split('-'))
